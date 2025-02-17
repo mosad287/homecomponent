2c7aa2a80403310e4b4d4dc57603f3de2b4ada98
mosad287 <mosadalbarbary832@gmail.com>
2025-02-17 17:41:43 +0200

    therd

--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import MainCart from "./mainCart";
+import MainCart from "./MainCart";
 import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../context/cart/CartContext";
 import { LoginContext } from "../../context/login/LoginContext";
